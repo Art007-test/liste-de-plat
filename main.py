dd01411842cb9addc2f8be2ee11be0fcaa0f15e2
Art007 <121630254+Art007-test@users.noreply.github.com>
2026-09-17 18:44:04 +0200

    Rajout des autres info a la description

--- main.py
+++ main.py
@@ -157,14 +157,22 @@ def search_from_list(search,dishes,topic):
 
 #################################################################################################
 # une ligne qui permet de faire un separateur
-def create_separator():
+def create_separator(thickness):
     separator = QFrame()
     separator.setFrameShape(QFrame.Shape.HLine)
     separator.setFrameShadow(QFrame.Shadow.Plain)
-    separator.setFixedHeight(1)
+    separator.setFixedHeight(thickness)
     return separator
 
-def cooking_time_to__text(minute):
+#prend le temp en minute et rend le joooli
+def cooking_time_to_text(minute):
+    
+    #si l'utilisateur n'a pa rentrer un chiffre
+    try:
+        minute = int(minute)
+    except:
+        return minute
+    
     #si le temp est en dessous d'une heure
     if minute < 60:
         return str(minute) + "min"
@@ -172,8 +180,14 @@ def cooking_time_to__text(minute):
     minute = minute % 60
     return str(hour) + "h " + str(minute) + "min"    
     
-    
-    
+#rend les listes jooooli
+def pretty_list(lst):
+    result = ""
+    for i in range(len(lst)):
+        result = result + "- " + str(lst[i])
+        if i != len(lst):
+            result = result + "\n"
+    return result
     
     
         
@@ -212,15 +226,66 @@ class MainWindow(QMainWindow):
         # Liste des plats
         self.dish_list = QListWidget()
 
-        # Informations du plat sélectionné
+
+
+        # Le titre
+        dish_title_layout = QHBoxLayout()
+        dish_title_layout.setContentsMargins(0,0,0,0)
+        
         self.dish_name = QLabel("Sélectionnez un plat")
-        self.dish_name.setStyleSheet(
-            "font-size: 24px; font-weight: bold;"
-        )
+        self.dish_name.setStyleSheet("font-size: 24px; font-weight: bold;")
+        
+        self.dish_mark = QLabel("")
+        self.dish_mark.setStyleSheet("font-size: 20px; font-weight: bold;")
+        
+        self.dish_time = QLabel("")
+        self.dish_time.setStyleSheet("font-size: 20px; font-weight: bold;")
+        
+        
+        dish_title_layout.addWidget(self.dish_mark,alignment=Qt.AlignmentFlag.AlignLeft)
+        
+        dish_title_layout.addWidget(self.dish_name,alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
+        
+        dish_title_layout.addWidget(self.dish_time,alignment=Qt.AlignmentFlag.AlignRight)
+        
+        
+        
+        
+        #la description du plat
+        self.dish_description_container = QWidget()
+        self.description_layout = QVBoxLayout(self.dish_description_container)
+        self.description_layout.setContentsMargins(0,0,0,0)
         
         self.dish_description = QLabel("")
         self.dish_description.setWordWrap(True)
         self.dish_description.setStyleSheet("font-size: 16px;")
+        
+        
+        self.description_layout.addWidget(self.dish_description)
+        self.description_layout.addWidget(create_separator(1))
+        
+        
+        
+        
+        
+        #les ingrédients
+        self.dish_necessary_ingredients = QLabel("")
+        self.dish_necessary_ingredients.setWordWrap(True)
+        self.dish_necessary_ingredients.setStyleSheet("font-size: 16px;")
+        
+        self.dish_unnecessary_ingredients = QLabel("")
+        self.dish_unnecessary_ingredients.setWordWrap(True)
+        self.dish_unnecessary_ingredients.setStyleSheet("font-size: 16px;")
+        
+        
+        
+        #la recette
+        self.dish_recipe = QLabel("")
+        self.dish_recipe.setWordWrap(True)
+        self.dish_recipe.setStyleSheet("font-size: 16px;")
+        self.dish_recipe_separator = create_separator(1)
+        
+        
         
         ####################################################################################################
         # Ajout des widgets
@@ -245,17 +310,39 @@ class MainWindow(QMainWindow):
         #la liste a gauche
         content_layout.addWidget(self.dish_list, 1)
         
+        #####################################
         #la boite à droite
         right_layout = QVBoxLayout()
         
+        ingredients_layout = QHBoxLayout()
+        
         right_layout.addStretch(1)
         
-        right_layout.addWidget(self.dish_name,alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
+        
+        
+        right_layout.addLayout(dish_title_layout)
         
         right_layout.addSpacing(20)
-        right_layout.addWidget(create_separator())
+        right_layout.addWidget(create_separator(5))
         
-        right_layout.addWidget(self.dish_description)
+        right_layout.addWidget(self.dish_description_container)
+        
+        
+        
+        
+        #ingredient layout
+        ingredients_layout.addWidget(self.dish_necessary_ingredients,alignment=Qt.AlignmentFlag.AlignTop)
+        ingredients_layout.addStretch(1)
+        ingredients_layout.addWidget(self.dish_unnecessary_ingredients,alignment=Qt.AlignmentFlag.AlignTop)
+        ingredients_layout.addStretch(2)
+        
+        right_layout.addLayout(ingredients_layout)
+        
+        
+        right_layout.addStretch(5)
+        
+        right_layout.addWidget(self.dish_recipe_separator)
+        right_layout.addWidget(self.dish_recipe)
         
         right_layout.addStretch(10)
         
@@ -264,6 +351,7 @@ class MainWindow(QMainWindow):
         
         main_layout.addLayout(content_layout)
 
+        ####################################################
         # Remplissage de la liste
         self.search("")
 
@@ -293,11 +381,66 @@ class MainWindow(QMainWindow):
         if current_item is None:
             self.dish_name.setText("Aucun plat avec cette recherche")
             self.dish_description.setText("")
+            self.dish_recipe.setText("")
+            self.dish_necessary_ingredients.setText("")
+            self.dish_unnecessary_ingredients.setText("")
+            
+            self.dish_recipe_separator.setVisible(False)
+            self.dish_description_container.setVisible(False)
+            
+            self.dish_time.setText("")
+            self.dish_mark.setText("")
+            
             return
+        
+        #recupere la recherche
         dish = current_item.data(Qt.ItemDataRole.UserRole)
         
-        self.dish_name.setText(dish["name"] + "       " + cooking_time_to__text(dish["cooking_time"]))
-        self.dish_description.setText("Description: \n" + dish.get("description","")) #met du vide si la description n'existe pas
+        self.dish_name.setText(dish["name"])
+        
+        
+        if dish.get("cooking_time","") != "":
+            if dish["cooking_time"] != 0:
+                self.dish_time.setText(cooking_time_to_text(dish["cooking_time"]))
+            else:
+                self.dish_time.setText("")
+        else:
+            self.dish_time.setText("")
+        
+        
+        if dish.get("mark","") != "":
+            self.dish_mark.setText(str(dish["mark"]) + "/10")
+        else:
+            self.dish_mark.setText("")
+            
+            
+        
+        #la description, met rien si la description est vide
+        if dish.get("description","") != "":
+            self.dish_description_container.setVisible(True)
+            self.dish_description.setText("• Description: \n" + dish.get("description","")+ "\n\n\n") #met du vide si la description n'existe pas
+        else:
+            self.dish_description_container.setVisible(False)
+            self.dish_description.setText("")
+        
+        
+        #la recette, met rien si la description est vide
+        if dish.get("recipe","") != "":
+            self.dish_recipe.setText("• Recette: \n" + dish.get("recipe",""))
+            self.dish_recipe_separator.setVisible(True)
+        else:
+            self.dish_recipe_separator.setVisible(False)
+            self.dish_recipe.setText("")
+            
+        if dish.get("necessary_ingredients",[]) != []:
+            self.dish_necessary_ingredients.setText("• Ingrédients: \n" + pretty_list(dish.get("necessary_ingredients",[])))
+        else:
+            self.dish_necessary_ingredients.setText("")
+        
+        if dish.get("unnecessary_ingredients",[]) != []:
+            self.dish_unnecessary_ingredients.setText("• Ingrédients supplémentaires: \n" + pretty_list(dish.get("unnecessary_ingredients",[])))
+        else:
+            self.dish_unnecessary_ingredients.setText("")
 
 
 
